170f71a4bee22206c1abe150cc5e9c2b63f41f4e
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-05-13 07:49:58 +0300

    add request form

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 const API_KEY = "f778cdcb7c824caabea32221231205";
 
-fetch("url.url.com/api", {
-  mode: "cors",
-});
-
 async function getCityData(city) {
   const response = await fetch(
     `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city.toLowerCase()}`
@@ -13,3 +9,10 @@ async function getCityData(city) {
 }
 
 getCityData("London");
+
+const form = document.querySelector("form.area");
+const areaInput = document.querySelector("input#area");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getCityData(areaInput.value);
+});
