59b6c1ea62b16498d2387e7071b17dfe5c11592b
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-05-14 01:26:48 +0300

    Change data, Annotate code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import "./style.css";
 
 const API_KEY = "f778cdcb7c824caabea32221231205";
@@ -5,6 +6,7 @@ const API_KEY = "f778cdcb7c824caabea32221231205";
 let currentWeather;
 let currentUnit;
 
+// fetch and retrieve data
 async function getCityData(city) {
   const response = await fetch(
     `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city.toLowerCase()}`
@@ -12,17 +14,29 @@ async function getCityData(city) {
   const data = await response.json();
   console.log(data);
   const { country, name } = data.location;
-  const { condition, icon } = data.current.condition;
-  const { tempC, tempF } = data.current;
-  return { country, name, condition, icon, tempC, tempF };
+  const { condition, code } = data.current.condition;
+  const { temp_c, temp_f, is_day } = data.current;
+  return { country, name, condition, code, temp_c, temp_f, is_day };
 }
+// Interface
+const firstRow = document.querySelector("div.first");
+const secondRow = document.querySelector("div.second");
+const thirdRow = document.querySelector("div.third");
+
+// main function
 async function handleCall(city) {
   currentWeather = await getCityData(city);
   console.log("here", currentWeather);
+
+  const icon = document.createElement("img");
+  icon.src = currentWeather.icon;
+
+  secondRow.appendChild(icon);
 }
 
+// call on page load
 handleCall("cairo");
-
+// call on input
 const form = document.querySelector("form.area");
 const areaInput = document.querySelector("input#area");
 form.addEventListener("submit", (e) => {
