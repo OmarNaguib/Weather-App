300f6cb57c0ae7b75f61f60685d09af3c35cf7d3
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-05-14 05:15:04 +0300

    Display content

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 const API_KEY = "f778cdcb7c824caabea32221231205";
 // state variables
 let currentWeather;
-let currentUnit;
+const currentUnit = "c";
 
 // fetch and retrieve data
 async function getCityData(city) {
@@ -14,24 +14,51 @@ async function getCityData(city) {
   const data = await response.json();
   console.log(data);
   const { country, name } = data.location;
-  const { condition, code } = data.current.condition;
+  const { text, code } = data.current.condition;
   const { temp_c, temp_f, is_day } = data.current;
-  return { country, name, condition, code, temp_c, temp_f, is_day };
+  return {
+    country,
+    name,
+    text,
+    code,
+    temp_c,
+    temp_f,
+    is_day,
+    getTemp() {
+      return currentUnit === "c" ? this.temp_c : this.temp_f;
+    },
+  };
 }
 // Interface
 const firstRow = document.querySelector("div.first");
 const secondRow = document.querySelector("div.second");
 const thirdRow = document.querySelector("div.third");
 
+function getImagePath() {
+  let path = "../assets/weather/64x64/";
+  if (currentWeather.is_day) path += "day/";
+  else path += "night/";
+  path += `${currentWeather.code}.png`;
+  return path;
+}
+
+function populateContainer() {
+  firstRow.textContent = `${currentWeather.name}, ${currentWeather.country}`;
+
+  secondRow.textContent = `${currentWeather.getTemp()}°`;
+
+  const icon = document.createElement("img");
+  icon.src = getImagePath();
+  secondRow.appendChild(icon);
+
+  thirdRow.textContent = currentWeather.text;
+}
+
 // main function
 async function handleCall(city) {
   currentWeather = await getCityData(city);
-  console.log("here", currentWeather);
 
-  const icon = document.createElement("img");
-  icon.src = currentWeather.icon;
-
-  secondRow.appendChild(icon);
+  populateContainer();
 }
 
 // call on page load
